105cf2aab84eef51987ce9a2fae4d22e538d586f
Dani Schuhman <dani8439@aol.com>
2022-11-11 15:03:47 -0500

    Create action - save newly created articles.

--- alpha_blog/app/controllers/articles_controller.rb
+++ alpha_blog/app/controllers/articles_controller.rb
@@ -15,6 +15,10 @@ class ArticlesController < ApplicationController
   end 
 
   def create 
-    render plain: params[:article]
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    # render plain: @article.inspect
+    @article.save 
+    # redirect_to article_path(@article)
+    redirect_to @article 
   end 
 end 
