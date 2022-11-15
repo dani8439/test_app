46176a081ccd2e6dc55eb94f3adf331a09330342
Dani Schuhman <dani8439@aol.com>
2022-11-15 14:47:18 -0500

    DRY up code and rendering partials.

--- alpha_blog/app/controllers/articles_controller.rb
+++ alpha_blog/app/controllers/articles_controller.rb
@@ -1,9 +1,7 @@
 class ArticlesController < ApplicationController 
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
   
   def show 
-    # instance variable so it's available within the view
-    # debugger
-    @article = Article.find(params[:id])
   end
 
   def index 
@@ -15,11 +13,10 @@ class ArticlesController < ApplicationController
   end 
 
   def edit 
-    @article = Article.find(params[:id])
   end
 
   def create 
-    @article = Article.new(params.require(:article).permit(:title, :description))
+    @article = Article.new(article_params)
     if @article.save 
       flash[:notice] = "Article was created successfully."
       redirect_to @article 
@@ -29,8 +26,7 @@ class ArticlesController < ApplicationController
   end  
 
   def update 
-    @article = Article.find(params[:id])
-    if @article.update(params.require(:article).permit(:title, :description))
+    if @article.update(article_params)
       flash[:notice] = "Article was updated successfully."
       redirect_to @article
     else 
@@ -39,8 +35,17 @@ class ArticlesController < ApplicationController
   end 
 
   def destroy 
-    @article = Article.find(params[:id])
     @article.destroy
     redirect_to articles_path
   end
+
+  private 
+
+  def set_article
+    @article = Article.find(params[:id])
+  end 
+
+  def article_params 
+    params.require(:article).permit(:title, :description)
+  end 
 end 
